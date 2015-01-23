823a68790c7884c7d2ea1547cb74080660e77f85
laurenpawell <lauren.pawell@gmail.com>
2015-01-22 18:06:06 -0800

    Add clickable behavior to squares in gameboard grid

--- script/script.js
+++ script/script.js
@@ -1,6 +1,63 @@
 // This is the Javascript required for the simple game logic behind Tic Tac Toe.
 
-// This allows you to determine who's turn it is
+// This is a reaction to a click on any of the squares within the board grid.
+
+	// Assign variables to each square within the board grid.
+	var squareOne = document.getElementById("square1");
+	var squareTwo = document.getElementById("square2");
+	var squareThree = document.getElementById("square3");
+	var squareFour = document.getElementById("square4");
+	var squareFive = document.getElementById("square5");
+	var squareSix = document.getElementById("square6");
+	var squareSeven = document.getElementById("square7");
+	var squareEight = document.getElementById("square8");
+	var squareNine = document.getElementById("square9");
+
+	// Start to see if we can add behavior to squareOne on click.
+
+	squareOne.onclick = function(){
+		console.log("One works!");
+	}
+
+	squareTwo.onclick = function(){
+		console.log("Two works!");
+	}
+
+	squareThree.onclick = function(){
+		console.log("Three works!");
+	}
+
+	squareFour.onclick = function(){
+		console.log("Four works!");
+	}
+
+	squareFive.onclick = function(){
+		console.log("Five works!");
+	}
+
+	squareSix.onclick = function(){
+		console.log("Six works!");
+	}
+
+	squareSeven.onclick = function(){
+		console.log("Seven works!");
+	}
+	squareEight.onclick = function(){
+		console.log("Eight works!");
+	}
+	squareNine.onclick = function(){
+		console.log("Nine works!");
+	}
+
+
+
+
+
+
+
+
+
+// This allows you to determine whose turn it is
 count = 1;
 count % 2 == 0 // Player 1's turn;
 count % 2 != 0 // Player 2's turn;
